6ff7aacf48389310dbaa76f04103e928482c058a
Masato Shimizu <ma6ato@gmail.com>
2014-11-26 18:04:16 +0900

    some environment check

--- deploy.py
+++ deploy.py
@@ -13,14 +13,8 @@ import argparse
 import paramiko
 paramiko.util.log_to_file("salt.log")
 
-ACCESS_KEY=os.environ.get('IDCF_COMPUTE_API_KEY')
-SECRET_KEY=os.environ.get('IDCF_COMPUTE_SECRET_KEY')
-HOST=os.environ.get('IDCF_COMPUTE_HOST')
 PATH='/client/api'
-SSH_KEY_FILE=os.environ.get('IDCF_SSH_KEY_FILE')
-
 SSH_IF='private_ips'
-
 SALT_NODES=['salt','minion1','minion2']
 IMAGE_NAME=r'Ubuntu Server 14.04'
 SIZE_NAME='light.S1'
@@ -29,13 +23,37 @@ class Salt(object):
 
     def __init__(self):
         self.cls = get_driver(Provider.CLOUDSTACK)
-        self.driver = self.cls(key=ACCESS_KEY,
-                               secret=SECRET_KEY,
-                               host=HOST,
-                               path=PATH)
+        self.create_driver()
         self.default_offering()
         self.list_nodes()
-        self.exit_if_vm_exists()
+
+    def create_driver(self):
+        access_key = os.environ.get('IDCF_COMPUTE_API_KEY')
+        secret_key = os.environ.get('IDCF_COMPUTE_SECRET_KEY')
+        host = os.environ.get('IDCF_COMPUTE_HOST')
+        self.ssh_key_file = os.environ.get('IDCF_SSH_KEY_FILE')
+
+        if not access_key:
+            print_exit('IDCF_COMPUTE_API_KEY')
+        if not secret_key:
+            print_exit('IDCF_COMPUTE_SECRET_KEY')
+        if not host:
+            print_exit('IDCF_COMPUTE_HOST')
+        try:
+            self.ssh_key = os.path.join(
+                os.path.expanduser('~'),
+                '.ssh',
+                self.ssh_key_file)
+        except:
+            print_exit('IDCF_SSH_KEY_FILE')
+
+        if not os.path.exists(self.ssh_key):
+            print_exit('{0} is not found'.format(self.ssh_key))
+
+        self.driver = self.cls(key=access_key,
+                               secret=secret_key,
+                               host=host,
+                               path=PATH)
 
     def default_offering(self):
         p = re.compile(IMAGE_NAME)
@@ -44,11 +62,11 @@ class Salt(object):
         self.size = [s for s in self.driver.list_sizes()
                         if s.name == SIZE_NAME][0]
         keynames = [k for k in self.driver.list_key_pairs()
-                        if k.name == SSH_KEY_FILE]
+                        if k.name == self.ssh_key_file]
         if len(keynames) > 0:
             self.keyname = keynames[0].name
         else:
-            print('{0} key name does not exisit in cloud'.format(SSH_KEY_FILE))
+            print('{0} key name does not exisit in cloud'.format(self.ssh_key_file))
             exit(1)
 
     def list_nodes(self):
@@ -63,16 +81,17 @@ class Salt(object):
     def exit_if_vm_exists(self):
         for n in self.nodes:
             if n.name in SALT_NODES:
-                print('{0} already exists, please destroy this vm beforehand')
+                print('{0} already exists, please destroy this vm beforehand'.format(n.name))
                 sys.exit(1)
 
-    def destroy(self,name):
+    def destroy(self):
         for n in self.nodes:
-            if name == n.name and n.name in SALT_NODES:
+            if n.state == 'RUNNING' and n.name in SALT_NODES:
                 retval = self.driver.destroy_node(n)
                 print('{0} is destoyed: {1}'.format(name,retval))
 
     def deploy(self,name,bootstrap):
+        self.exit_if_vm_exists()
         start = time.time()
         print('start {0}'.format(name))
         script = ScriptDeployment(bootstrap)
@@ -80,14 +99,13 @@ class Salt(object):
         node = self.driver.deploy_node(name=name,
                                   image=self.image,
                                   size=self.size,
-                                  ssh_key=SSH_KEY_FILE,
+                                  ssh_key=self.ssh_key,
                                   ex_keyname=self.keyname,
                                   ssh_interface=SSH_IF,
                                   deploy=msd)
         end = time.time()
         elapsed = end - start
         print('end {0}, eplapsed: {1}'.format(name,elapsed))
-
 
 def action(args):
 
@@ -98,7 +116,7 @@ curl -L http://bootstrap.saltstack.com | sh -s -- -M
 curl -L http://bootstrap.saltstack.com | sh
 '''
     salt = Salt()
-    command = args.command[0]
+    command = args.command
     if command == 'deploy':
         salt.deploy('salt',master_bootstrap)
         for i in range(1, 3):
@@ -107,7 +125,7 @@ curl -L http://bootstrap.saltstack.com | sh
     elif command == 'list':
         salt.print_nodes()
     elif command == 'destroy':
-        salt.destroy(command[1])
+        salt.destroy()
     else:
         print 'env does not exists: {0}'.format(args.env)
         sys.exit(1)
@@ -115,7 +133,7 @@ curl -L http://bootstrap.saltstack.com | sh
 def parse_arguments():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("command",type=str,help="Simple Salt Command",
-                        default='list', nargs='+',
+                        default='list', 
                         choices=["deploy","destroy","list"])
     args = parser.parse_args()
     return args
@@ -128,24 +146,5 @@ def print_exit(msg):
     print('please set {0} in your environment'.format(msg))
     sys.exit(1)
 
-def check_environments():
-    if not ACCESS_KEY:
-        print_exit('IDCF_COMPUTE_API_KEY')
-    if not SECRET_KEY:
-        print_exit('IDCF_COMPUTE_SECRET_KEY')
-    if not HOST:
-        print_exit('IDCF_COMPUTE_HOST')
-        print_exit('IDCF_SSH_KEY_FILE')
-    try:
-        ssh_key_file=os.path.join(
-            os.path.expanduser('~'),
-            '.ssh',
-            SSH_KEY_FILE)
-        if not os.path.exists(ssh_key_file):
-            print_exit('{0} is not found'.format(ssh_key_file))
-    except:
-        print_exit('IDCF_SSH_KEY_FILE')
-        
 if __name__ == "__main__":
-    check_environments()
     main()
